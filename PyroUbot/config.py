92da59c84b636235038cb3310a975a04a21ad1c0
Azello-XD <jonathanlenathea@gmail.com>
2025-04-27 14:44:08 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -11,7 +11,7 @@ API_ID = int(os.getenv("API_ID", "27418440"))
 
 API_HASH = os.getenv("API_HASH", "0a08a360e0e9f41b9896f655c300d09d")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7856214120:AAEc_rJ2CUvkhZepUK__3rZjxzpBASsv-4M")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7856214120:AAEc_rJ2CUvkhZepUK_3rZjxzpBASsv-4M")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "5662169739"))
 
